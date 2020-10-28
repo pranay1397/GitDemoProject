92cb080ac64ff0c6f9d77cc4b4977492d28d8de3
prana <prana@192.168.1.72>
2020-10-28 03:16:32 -0400

    First Commit

---
+++ src/testclass/Test1.java
@@ -0,0 +1,16 @@
+package testclass;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Pranay";
+		
+		if(name == "Pranay")
+			System.out.println("You are awesome");
+		else {
+			System.out.println("Pranay is best");
+		}
+	}
+
+}
